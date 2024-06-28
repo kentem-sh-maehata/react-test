02dff16ab7d80ad25c438882b39f4e4415800ddc
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-06-28 11:40:22 +0900

    Q4.単体テスト

---
+++ my-book-register/src/test/App.test.tsx
@@ -0,0 +1,59 @@
+import '@testing-library/jest-dom/vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import App from '../App';
+
+beforeEach(() => {
+  render(<App />);
+});
+afterEach(() => {
+  cleanup();
+});
+
+const user = userEvent.setup();
+
+describe('本のテーブル', () => {
+  beforeEach(async () => {
+    const textboxes = screen.getAllByRole('textbox');
+    await user.type(textboxes[0], '9784295007333');
+    await user.click(screen.getByRole('button', { name: '書籍登録' }));
+    expect(
+      await screen.findByText('1週間でC#の基礎が学べる本'),
+    ).toBeInTheDocument();
+  });
+  test('書籍が登録できており、利用可能になっている', async () => {
+    expect(screen.getByText('利用可能')).toBeInTheDocument();
+  });
+  test('貸し出しボタンを押すと利用状況が切り替わる', async () => {
+    await user.click(screen.getByRole('button', { name: '貸出' }));
+    expect(screen.getByText('貸出中'));
+  });
+  test('同じ本を登録することができない', async () => {
+    const bookList = screen.getAllByText('1週間でC#の基礎が学べる本');
+    expect(bookList.length).toBe(1);
+  });
+  test('返却ボタンを押すと利用状況が利用可能に切り替わる', async () => {
+    await user.click(screen.getByRole('button', { name: '貸出' }));
+    await user.click(screen.getByRole('button', { name: '返却' }));
+    expect(screen.getByText('利用可能')).toBeInTheDocument();
+  });
+  test('削除ボタンを押すと本が削除される', async () => {
+    await user.click(screen.getByRole('button', { name: '削除' }));
+    expect(
+      screen.queryByText('1週間でC#の基礎が学べる本'),
+    ).not.toBeInTheDocument();
+  });
+  test('フィルターに値を入力すると絞り込まれる', async () => {
+    const textboxes = screen.getAllByRole('textbox');
+    await user.type(textboxes[1], 'C');
+    expect(screen.getByText('1週間でC#の基礎が学べる本')).toBeInTheDocument();
+  });
+  test('フィルターに関係ない値を入力したら何も表示されない', async () => {
+    const textboxes = screen.getAllByRole('textbox');
+    await user.type(textboxes[1], 'A');
+    expect(
+      screen.queryByText('1週間でC#の基礎が学べる本'),
+    ).not.toBeInTheDocument();
+  });
+});
